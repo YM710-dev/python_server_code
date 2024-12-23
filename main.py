4a5ab27edc183e1c067f8238fb29ead47ed6c753
YM710-dev <yacineman710@gmail.com>
2024-12-23 17:09:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     message = request.data.decode()  # Decode the incoming message
     print(f"ESP says: {message}")
-    return "Hello"  # Respond with a simple message
+    return "Hello " + message  # Respond with a simple message
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
